818195aa90d257ed404aa6c39dd24aff52c29e24
kurtiskusz <kurtiskusz@gmail.com>
2017-10-20 10:35:43 -0400

    removed lombok dependency

--- app/src/main/java/com/mobileapps/walkbuddy/models/User.java
+++ app/src/main/java/com/mobileapps/walkbuddy/models/User.java
@@ -2,13 +2,10 @@ package com.mobileapps.walkbuddy.models;
 
 import com.google.firebase.database.IgnoreExtraProperties;
 
-import lombok.Value;
-
 /**
  * Created by kurti on 10/5/2017.
  */
 @IgnoreExtraProperties
-@Value
 public class User {
     private static final String DEFAULT_NAME = "name";
     private static final String DEFAULT_EMAIL = "email";
@@ -24,5 +21,13 @@ public class User {
     public User(String name, String email) {
         this.name = name;
         this.email = email;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
     }
 }
