ae6f139dd654dc979ae712aa6f3271bb12e78baf
utomo <utomorezeki@gmail.com>
2017-11-04 21:38:20 -0400

    added dest map frag

---
+++ app/src/main/java/com/mobileapps/walkbuddy/DestinationMapFrag.java
@@ -0,0 +1,75 @@
+package com.mobileapps.walkbuddy;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.mobileapps.walkbuddy.models.Route;
+import com.mobileapps.walkbuddy.walkbuddy.R;
+
+import java.util.ArrayList;
+
+public class DestinationMapFrag extends Fragment implements OnMapReadyCallback{
+    MapView mMapView;
+    GoogleMap mGoogleMap;
+    View mView;
+    public Route userR;
+
+    public DestinationMapFrag() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mView = inflater.inflate(R.layout.fragment_destination_map, container, false);
+        return mView;
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mMapView = (MapView) mView.findViewById(R.id.map);
+        if (mMapView != null){
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            userR = bundle.getParcelable("data");
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        mGoogleMap = googleMap;
+        googleMap.setMapType(googleMap.MAP_TYPE_NORMAL);
+        ArrayList<LatLng> userData = userR.getUserRoute();
+        Polyline polyline1 = googleMap.addPolyline(new PolylineOptions()
+                .clickable(false)
+                .addAll(userData));
+    }
+}
--- app/src/main/java/com/mobileapps/walkbuddy/models/Route.java
+++ app/src/main/java/com/mobileapps/walkbuddy/models/Route.java
@@ -1,5 +1,9 @@
 package com.mobileapps.walkbuddy.models;
 
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
 /**
  * Created by kurti on 11/2/2017.
  */
@@ -8,7 +12,8 @@ public class Route {
     private String routeName;
     private String startLocationName;
     private long timeInMillis;
-
+    private ArrayList<Double> verticesLat;
+    private ArrayList<Double> verticesLng;
     public Route() {}
 
     public Route(String routeName, String startLocationName, long timeInMillis) {
@@ -17,6 +22,13 @@ public class Route {
         this.timeInMillis = timeInMillis;
     }
 
+    public ArrayList<LatLng> getUserRoute(){
+        ArrayList<LatLng> result = new ArrayList<LatLng>();
+        for(int i = 0; i < verticesLat.size(); i++){
+            result.add(new LatLng(verticesLat.get(i),verticesLng.get(i)));
+        }
+        return result;
+    }
     public String getRouteName() { return this.routeName; }
 
     public String getStartLocationName() { return this.startLocationName; }
